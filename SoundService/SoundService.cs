7419c2db3e17681ccac9f948caa5a32cd52e5f97
natedolz <natedolz@gmail.com>
2018-07-20 23:39:15 -0500

    End of day commit

--- SoundService/SoundService.cs
+++ SoundService/SoundService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
+using System.Text.RegularExpressions;
 
 
 namespace SoundService
@@ -10,38 +12,44 @@ namespace SoundService
         private static SpeechFactory factory;
         private static SpeechFactory Factory = factory ?? (factory = SpeechFactory.FromSubscription(Constants.APIKey1, Constants.Region));
 
-        public async Task KickOffListener()
+        public void KickOffListener()
         {
             var recognizer = Factory.CreateSpeechRecognizer();
 
-            recognizer.RecognitionErrorRaised += RecognitionErrorRaised;=
-            recognizer.IntermediateResultReceived += IntermediateResultReceived;
+            recognizer.RecognitionErrorRaised += RecognitionErrorRaised;
+            recognizer.FinalResultReceived += FinalResultRecieved;
             #pragma warning disable CS4014 //Intent Is To leave this open in the background
             recognizer.StartContinuousRecognitionAsync();
             #pragma warning restore CS4014 //Intent Is to leave this open in the background
-
-
-
         }
 
-        private void IntermediateResultReceived(object sender, SpeechRecognitionResultEventArgs e)
-        {            
-            var result = e.Result.Text;
-            KeywordRecognized(result);
-        }
-
-        private void OnSpeechDetectedEvent(object sender, RecognitionEventArgs e)
+        private void FinalResultRecieved(object sender, SpeechRecognitionResultEventArgs e)
         {
-            
+            var result = e.Result.Text;
+            KeywordRecognized(result);        
         }
 
         private void RecognitionErrorRaised(object sender, RecognitionErrorEventArgs e)
         {
-            throw new NotImplementedException();
+            
         }
 
         public delegate void KeywordRecognizedDelegate(string keyWord);
         public KeywordRecognizedDelegate KeywordRecognized { get; set; }
-        
+
+        private KeyValuePair<string, string> ProcessSpeachResult(string speech)
+        {
+            speech = speech.Replace("?", "");
+            speech = speech.Replace(".", "");
+            speech = speech.Replace(",", "");
+            speech = speech.Replace(" ", "");
+            speech = speech.ToLower();
+
+            var showExpression = new Regex(@"(?:^|\W)showme(?:$|\W)");
+            var expandRegex = new Regex(@"(?:^|\W)expand(?:$|\W)");
+            
+        }
+
+
     }
 }
