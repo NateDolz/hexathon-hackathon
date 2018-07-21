ff4ff8f823607ea81b78aa4d78fa0110f7d17705
natedolz <natedolz@gmail.com>
2018-07-21 15:44:23 -0500

    Log Commands in COnsole

--- SoundServiceTestApp/Program.cs
+++ SoundServiceTestApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using SoundService;
 
 namespace SoundServiceTestApp
@@ -8,20 +9,64 @@ namespace SoundServiceTestApp
         static void Main(string[] args)
         {
             var soundService = new SoundService.SoundService();
-            
-            soundService.ShowItem = item => Console.WriteLine("Show " + item);
-            soundService.RemoveItem = item => Console.WriteLine("Remove " + item);
-            soundService.TurnItem = (item , direction) => Console.WriteLine("Turn " + item + " " + direction.ToString());
-            soundService.MoveItem = (item, direction) => Console.WriteLine("Move " + item + " " + direction.ToString());
-            soundService.ExpandItem = item => Console.WriteLine("Expand " + item);
-            soundService.CollapseItem = item => Console.WriteLine("Collapse " + item);
-            soundService.Faster = () => Console.WriteLine("Faster");            
-            soundService.Slower = () => Console.WriteLine("Slower");
-            soundService.Stop = () => Console.WriteLine("Stop");
+
+            var tempDirectory = Path.GetTempPath();
+            var filePath = tempDirectory + @"\SpeechCommands.txt";
+            var streamWriter = new StreamWriter(filePath);
+            streamWriter.AutoFlush = true;
+
+            soundService.ShowItem = item =>
+            {
+                Console.WriteLine("Show " + item);
+                streamWriter.WriteLine("Show " + item);
+            };
+            soundService.RemoveItem = item =>
+            {
+                Console.WriteLine("Remove " + item);
+                streamWriter.WriteLine("Remove " + item);
+            };
+            soundService.TurnItem = (item , direction) =>
+            {
+                Console.WriteLine("Turn " + item + " " + direction.ToString());
+                streamWriter.WriteLine("Turn " + item + " " + direction.ToString());
+            };
+            soundService.MoveItem = (item, direction) =>
+            {
+                Console.WriteLine("Move " + item + " " + direction.ToString());
+                streamWriter.WriteLine("Move " + item + " " + direction.ToString());
+            };
+            soundService.ExpandItem = item =>
+            {
+                Console.WriteLine("Expand " + item);
+                streamWriter.WriteLine("Expand " + item);
+            };
+            soundService.CollapseItem = item =>
+            {
+                Console.WriteLine("Collapse " + item);
+                streamWriter.WriteLine("Collapse " + item);
+            };
+            soundService.Faster = () =>
+            {
+                Console.WriteLine("Faster");
+                streamWriter.WriteLine("Faster");
+            };            
+            soundService.Slower = () =>
+            {
+                Console.WriteLine("Slower");
+                streamWriter.WriteLine("Slower");
+            };
+            soundService.Stop = () =>
+            {
+                Console.WriteLine("Stop");
+                streamWriter.WriteLine("Stop");
+            };
 
             Console.WriteLine("Start Talking...");
             soundService.KickOffListener();
             Console.ReadLine();
+
+            streamWriter.Close();
+            soundService.Dispose();
         }
     }
 }
