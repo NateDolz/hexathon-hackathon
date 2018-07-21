64964ed79c5e564f13fcb4f2fcbc272e3958e34a
natedolz <natedolz@gmail.com>
2018-07-21 11:41:31 -0500

    update gitignore

---
+++ SoundService/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoundService
+{
+    public static class Constants
+    {
+        public static string APIKey1 => "9ee338f6889d477a873423c94fb5739d";
+        public static string APIKey2 => "6f8757752ef249c29474ff40d3c79cb2";
+        public static string Region => "westus";
+    }
+}
